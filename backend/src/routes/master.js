60cda648b2fa0936e952a683870662214e9bfaef
priyabergithlibin-code <priyabergithlibin@gmail.com>
2026-02-13 15:35:47 +0530

    removed an unused file

--- backend/src/routes/master.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { seedMaster, listMaster } = require("../controllers/masterController");
-
-router.post("/seed", seedMaster);
-router.get("/list", listMaster);
-
-module.exports = router;
